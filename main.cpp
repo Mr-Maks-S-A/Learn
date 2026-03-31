2d997fcf2bf2e200a710e21c492404dff5e16027
MR-Maks-SA <mrfarkses@gmail.com>
2026-03-31 19:44:37 +0300

    Реализовано Одинаковые, но разные функции

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 /**
- * @mainpage Программа "Считающая функция"
+ * @mainpage Программа "Одинаковые, но разные функции"
  * @section desc Описание
- * Программа вызывает функцию в цикле,
- * а функция ведет внутренний учет количества вызовов
- * при помощи статичесской переменной.
- *
- * @note Примечание
- *
+ * Программа демонстрирует использование пространств имен (namespace) для создания
+ * функций с идентичными сигнатурами, выполняющих различные арифметические операции.
  *
  * @author Салакатов Максим Альбертович aka (Jarko || Mr-Maks-S-A)
  */
@@ -14,30 +10,55 @@
 #include <iostream> // Для потоков ввода/вывода (std::cout)
 #include <cstdlib>  // Для макроса EXIT_SUCCESS
 #include <clocale>  // Библиотека для работы с локалью (setlocale)
-// #include <type_traits> // Необходимо для std::is_copy_assignable и других проверок
+#include <type_traits> // Необходимо для // Для std::is_arithmetic
 
 
-/**
- * @brief Функция, отслеживающая количество своих вызовов.
- * * @details Использует внутреннюю статическую переменную @c counter.
- * При каждом вызове значение увеличивается на единицу и выводится в консоль.
- * * @return void
- */
-void counting_function() {
-    /** * @brief Статический счетчик вызовов.
-     * Инициализируется нулем только при первом обращении к функции.
-     */
-    static int counter = 0;
-    counter++;
-    std::cout << "Количество вызовов функции counting_function(): " << counter << std::endl;
+
+/** @namespace Addition Содержит реализацию сложения */
+namespace Addition {
+    template <typename T>
+    T calc(T x, T y) {
+        static_assert(std::is_arithmetic<T>::value, "Тип должен быть числовым!");
+        return x + y;
+    }
 }
+
+/** @namespace Subtraction Содержит реализацию вычитания */
+namespace Subtraction {
+    template <typename T>
+    T calc(T x, T y) {
+        static_assert(std::is_arithmetic<T>::value, "Тип должен быть числовым!");
+        return x - y;
+    }
+}
+
+/** @namespace Multiplication Содержит реализацию умножения */
+namespace Multiplication {
+    template <typename T>
+    T calc(T x, T y) {
+        static_assert(std::is_arithmetic<T>::value, "Тип должен быть числовым!");
+        return x * y;
+    }
+}
+
+/** @namespace Division Содержит реализацию деления */
+namespace Division {
+    template <typename T>
+    T calc(T x, T y) {
+        static_assert(std::is_arithmetic<T>::value, "Тип должен быть числовым!");
+        if (y == 0) return 0; // Базовая защита от деления на ноль
+        return x / y;
+    }
+}
+
 
 
 /**
  * @brief Точка входа в программу.
  * * @details Процесс работы:
- * 1. Запуск цикла от 0 до 14 (всего 15 итераций).
- * 2. Вызов функции counting_function() на каждой итерации.
+ * 1. Инициализация локали и тестовых переменных x и y.
+ * 2. Последовательный вызов функций calc из разных пространств имен.
+ * 3. Вывод результатов арифметических действий в консоль.
  *
  * @return int Статус завершения программы (EXIT_SUCCESS в случае успеха).
  */
@@ -48,9 +69,16 @@ int main() {
     #endif
 
 
-    for (int i = 0; i < 15; i++) {
-        counting_function();
-    }
+    int x = 6;
+    int y = 9;
+
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+
+    // Вызовы функций с указанием конкретного пространства имен
+    std::cout << "Сложение: " << Addition::calc(x, y) << std::endl;
+    std::cout << "Вычитание: " << Subtraction::calc(x, y) << std::endl;
+    std::cout << "Умножение: " << Multiplication::calc(x, y) << std::endl;
+    std::cout << "Деление: "   << Division::calc(x, y) << std::endl;
 
     return EXIT_SUCCESS;
 }
